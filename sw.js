38ce9c67fc9193ae4432ec8f87f37b461a47865c
higa-ka <yaeyama-th_2505@open.ed.jp>
2026-03-12 20:01:26 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+// キャッシュ名を上げると更新が確実に反映されます
+const CACHE_NAME = 'table-app-v2';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './static/table1.jpg',
+  './static/table2.jpg',
+  './static/table3.jpg',
+  './static/table4.jpg',
+  './static/table5.jpg',
+  './static/table6.jpg',
+  './static/icon-192.png',
+  './static/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request).catch(() => {
+        if (e.request.destination === 'image') {
+          // 画像取得失敗時のフォールバック
+          return caches.match('./static/table1.jpg');
+        }
+      });
+    })
+  );
+});
